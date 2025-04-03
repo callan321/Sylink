8b0f7f41a42bdeed41c0c7fd52bfee74c31ec3a1
Callan <callan.mitchell11@gmail.com>
2025-04-04 10:41:11 +1100

    fix: enable Tailwind CSS IntelliSense in WebStorm by adding minimal config

---
+++ ClientApp/tailwind.config.js
@@ -0,0 +1,9 @@
+// Tailwind CSS config added to enable IntelliSense support in WebStorm
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
