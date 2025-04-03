5aed12f4ebcfba4774aa7db3ed20e2b75390be8f
Callan <callan.mitchell11@gmail.com>
2025-04-03 12:50:00 +1100

    feat(client): Added for 404 page

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
-import {authRoutes} from './features/auth/auth.routes';
-
+import { authRoutes } from './features/auth/auth.routes';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent : () => import('./features/onboarding/landing-page/landing-page.component').then(m => m.LandingPageComponent) ,
+    loadComponent: () =>
+      import('./features/onboarding/landing-page/landing-page.component').then(
+        (c) => c.LandingPageComponent,
+      ),
   },
   {
-    path : 'auth',
-    loadChildren: () =>  import('./features/auth/auth.routes').then(r => authRoutes)
-  }
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((r) => authRoutes),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./core/components/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent,
+      ),
+  },
 ];
